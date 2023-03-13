b6057ba3869904127e79d99bae0e0f413f1e1c5e
Melusi Mkhize <msstudymail@gmail.com>
2023-03-13 23:24:05 +0200

    new cpp

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a,x;
+    scanf("%d %d ", &a,&x);
+    printf("a=%d x=%d\n",a,x);
+    printf("Melusi Mkhize, don't worry you will understand c programming");
+return(0);
+
+}
---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
+ */
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+   int a,x;
+   cin>>a>>x;
+   cout<<"Hi there Melusi, please enter two numbers: ";
+   cout<<a<<x;
+return 0;
+}
