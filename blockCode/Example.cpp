ff58a30b48f958a8882e0e060b3cd01cdedf940c
Melusi Mkhize <msstudymail@gmail.com>
2023-03-21 23:40:26 +0200

    I tried to use blocCode IDE to see how much I'm capable.

---
+++ blockCode/Example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+      int x,y;
+  cout<<"please enter x: ";
+  cin>>x;
+  cout<<endl;
+  cout<<"please enter y: ";
+  cin>>y;
+  cout<<endl;
+  cout <<"x + y = "<<x+y<<endl;
+  return 0;
+}
